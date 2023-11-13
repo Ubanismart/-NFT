5cce140dc700edddb69738ff72d3c2b014979f2e
Ubanismart <105659114+Ubanismart@users.noreply.github.com>
2023-11-13 04:08:45 +0000

    initialising

---
+++ sponge/src/spongebob.sol
@@ -0,0 +1,11 @@
+c// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155LazyMint.sol";
+
+contract spongebob is ERC1155LazyMint {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+    ) ERC1155LazyMint(_name,_symbol,msg.sender, 0) {}
+    
