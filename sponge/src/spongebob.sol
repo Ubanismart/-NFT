0490a08a55edf14b54e454a14ca180a418451c6b
Ubanismart <105659114+Ubanismart@users.noreply.github.com>
2023-11-14 04:31:14 +0000

    evolve contracts

--- sponge/src/spongebob.sol
+++ sponge/src/spongebob.sol
@@ -8,4 +8,16 @@ contract spongebob is ERC1155LazyMint {
         string memory _name,
         string memory _symbol,
     ) ERC1155LazyMint(_name,_symbol,msg.sender, 0) {}
-    
+    function verifyClaim(
+    address _claimer,
+    uint256 _tokenId,
+    uint256 _quantity)
+    public view override{ 
+    require(_tokenid ==0, "only SpongeBob are claimable ");
+    require(_quantity, "only 1 SpongeBob can be claimed");
+    }
+    function evolve(){
+    _burn(msg.sender,0,2);
+    _mint(msg.sender,1,1, "");
+    }
+}
